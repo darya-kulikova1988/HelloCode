8e4be767e269bc2f71d80798941eab3c1939db7a
Darya <darya.kulikova88@yandex.ru>
2022-12-03 23:01:52 +0300

    Example011_ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,8 +1,8 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray(int[] collection) // void - метод ничего не возвращает
 {
     int Length = collection.Length;
     int index = 0;
-    while (index < Length);
+    while (index < Length)
     {
         collection[index] = new Random().Next(1,10);
         // index = index + 1;
@@ -20,9 +20,30 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // если нет элемента
+    while (index < count)
+    {
+        if(collection[index]==find)
+       {
+        position = index;
+        break; // первое вхождение
+       }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int [10];
 
 FillArray(array);
+array[4]=4;
+array[6]=4;
 PrintArray(array);
+Console.WriteLine();
 
-
+int pos = IndexOf(array,4);
+Console.WriteLine(pos);

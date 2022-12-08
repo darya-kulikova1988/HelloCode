823f2e779e707ff1cd67e53874fef18cefaa029d
Darya <darya.kulikova88@yandex.ru>
2022-12-09 00:00:43 +0300

    Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -79,3 +79,108 @@ for (int i = 2; i <= 10; i++)
     }
     Console.WriteLine();
 }
+
+// // текст, работа с текстом
+// Дан текст. в тексте нужно все пробелы заменить черточками,
+// маленькте буквы к заменить большими К
+// а большие С заменить маленькми с.
+// Ясна ли задача?
+
+string text = " - Я думАю, - сказАл князь, улыбаясь, - что, "
+            + "новАя строка";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'А', 'а');
+Console.WriteLine(newText);
+
+// Алгоритм сортировки методом выбора - найти позицию минимального элемента
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr); // упорядочивание массива
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine();
+
+// максимальный элемент
+
+int[] arr1 = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray1(int[] array1)
+{
+    int count = array1.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort1(int[] array1)
+{
+    for (int i = 0; i < array1.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array1.Length; j++)
+        {
+            if (array1[j] > array1[maxPosition]) maxPosition = j;
+        }
+
+        int temporary1 = array1[i];
+        array1[i] = array1[maxPosition];
+        array1[maxPosition] = temporary1;
+    }
+}
+PrintArray1(arr1);
+SelectionSort1(arr1); // упорядочивание массива
+PrintArray1(arr1);

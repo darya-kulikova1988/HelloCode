316493ec74bbc2fb9464b8e2db8da4aeccabb13c
Darya <darya.kulikova88@yandex.ru>
2022-12-08 13:06:05 +0300

    Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -25,3 +25,33 @@ void Method21(string msg, int count)
     }
 }
 Method21("Текст", 4);
+// Method21(msg: "Новый текст", count: 4);
+// Method21(count: 4, msg: "самый текст");
+
+
+// вид 3 - возвращают, но не принимают (тип данных должны задать)
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+// вид 3 - возвращают и принимают 
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);

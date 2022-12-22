5833732df561193c035bb8662fb8247a20d24323
Darya <darya.kulikova88@yandex.ru>
2022-12-22 15:32:02 +0300

    лекция по напсианию кода

---
+++ Beautiful_code/Program.cs
@@ -0,0 +1,13 @@
+﻿void DrawText(string text, int left, int top)
+{
+    Console.SetCursorPosition(left, top);
+    Console.WriteLine(text);
+}
+
+DrawText("Intensive C# Demo text", 629, 360); // магические числа
+
+string caption = "Intensive C# Demo text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;  // left
+int screenHeightPosition = Console.WindowHeight / 2; // top
+
+DrawText(caption, screenWidthPosition, screenHeightPosition);

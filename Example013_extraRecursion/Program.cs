3ec0091c7bc1bb163e294d418c7dc26fa5596638
Darya <darya.kulikova88@yandex.ru>
2022-12-24 23:58:08 +0300

    рекурсия доп лекция

---
+++ Example013_extraRecursion/Program.cs
@@ -0,0 +1,77 @@
+﻿string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+
+string NumbersRec(int a, int b)
+{
+    if (a < b + 1) return $"{a} " + NumbersRec(a + 1, b);
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(1, 10));
+
+
+// сумма чисел от 1 до n
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+Console.WriteLine(SumFor(10));
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumRec(10));
+
+
+// число а возвести в степень n
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+Console.WriteLine(PowerFor(2, 10));
+
+int PowerRec(int a, int n)
+{
+    return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+    // if (n == 0) return 1;
+    // else return PowerRec(a, n - 1) * a;
+}
+Console.WriteLine(PowerRec(2, 10));
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowerRec(2, 10));
+
+char[] s = { 'а', 'и', 'с', 'в' };
+int count = s.Length;
+int n = 1;
+for (int i = 0; i < count; i++)
+{
+    for (int j = 0; j < count; j++)
+    {
+        Console.WriteLine($"{n++,-5} {s[i]} {s[j]}");
+    }
+}
+
